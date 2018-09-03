d35f0d90bed65226524922f4f463c2b31be684f8
Johan Svensson <plasmacheesejoj@gmail.com>
2018-09-03 17:08:31 +0200

    team hero

--- src/team/index.js
+++ src/team/index.js
@@ -21,45 +21,54 @@ const team = [{
  * @author Johan Svensson
  */
 export default class TeamScreen extends React.Component {
-  componentDidMount() {  
+  componentDidMount() {
     document.title = "Team - #6819 Viking Robotics";
   }
 
   render() {
     return (
-      <div className="padded">
-        <h1 style={{ margin: 0, marginBottom: 36 }}>Meet the Vikings</h1>
-        <ul id="team-list">
-          {
-            team.map((member, i) => (
-              <li className="member" key={i.toString()}>
-                <div className="picture" style={{
-                  backgroundImage: `url(${member.image})`
-                }}></div>
-                <div className="about">
-                  <h2 className="name">
-                    {
-                      member.name
-                    }
-                  </h2>
-                  <p className="role">
-                    <div className="icon"
-                      style={{ backgroundImage: `url(${member.roleIcon})` }}>
-                    </div>
-                    <span className="text">
+      <div>
+        <div id="team-hero">
+          <div className="bg"></div>
+          <img className="cover-image" alt="Team" src={require('../assets/team_hero.jpg')} />
+          <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+            <h1 className="heading" style={{ margin: 0, marginBottom: 36 }}>Meet the Vikings</h1>
+          </div>
+
+        </div>
+        <div className="padded">
+          <ul id="team-list">
+            {
+              team.map((member, i) => (
+                <li className="member" key={i.toString()}>
+                  <div className="picture" style={{
+                    backgroundImage: `url(${member.image})`
+                  }}></div>
+                  <div className="about">
+                    <h2 className="name">
                       {
-                        member.role
+                        member.name
                       }
-                    </span>
-                  </p>
-                  <a className="email-link" href={
-                    `mailto:${member.email}`
-                  }>Send me an ✉️</a>
-                </div>
-              </li>
-            ))
-          }
-        </ul>
+                    </h2>
+                    <p className="role">
+                      <div className="icon"
+                        style={{ backgroundImage: `url(${member.roleIcon})` }}>
+                      </div>
+                      <span className="text">
+                        {
+                          member.role
+                        }
+                      </span>
+                    </p>
+                    <a className="email-link" href={
+                      `mailto:${member.email}`
+                    }>Send me an ✉️</a>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
     )
   }
