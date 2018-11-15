5e670bc84a6a8e2d2c1d6c461219426ad656fe80
Johan Svensson <partlight.johans@gmail.co>
2018-11-16 00:06:13 +0100

    updated teams

--- src/team/index.js
+++ src/team/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 
 import './styles.scss';
 
-const team = [{
-  name: 'Member 1',
-  role: 'CAD',
-  roleIcon: require('../assets/team/icons/ic_programming.png'),
-  image: require('../assets/team/oscar.JPG'),
-  email: 'jojjedeveloper@gmail.com'
-}, {
-  name: 'Member 1',
-  role: 'Programmering',
-  roleIcon: require('../assets/team/icons/ic_programming.png'),
-  image: require('../assets/team/viktor.JPG'),
-  email: 'jojjedeveloper@gmail.com'
-}]
 
 /**
  * A screen for displaying team members.
@@ -34,42 +21,45 @@ export default class TeamScreen extends React.Component {
           <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
             <h1 className="heading" style={{ margin: 0, marginBottom: 36 }}>Meet the Vikings</h1>
           </div>
+        </div>
 
+        <div id="groups">
+          <div className="group-container" style={{ background: '#2E7D32' }}>
+            <div className="group" style={{ background: '#2E7D32' }}>
+              <div className="photo-container">
+                <div className="curtains"></div>
+                <img alt="Designers" src={require('../assets/team/designers.jpeg')} />
+              </div>
+              <h2 className="caption">Design</h2>
+              <p className="names"><span>Axel Löfqvist</span>, <span>William Johansson</span></p>
+              <p className="names">In front: <span>Ebba Jönsson</span>, <span>Ebba Bergström</span>, <span>Viktor Syrén</span>, <span>Daniel Odelgård</span></p>
+            </div>
+          </div>
+
+          <div className="group-container" style={{ background: '#56378e' }}>
+            <div className="group" style={{ background: '#56378e' }}>
+              <div className="photo-container">
+                <div className="curtains"></div>
+                <img alt="Designers" src={require('../assets/team/manufacturers.jpeg')} />
+              </div>
+              <h2 className="caption">Manufacturing</h2>
+              <p className="names">Left to right: <span>Louise Lindgren</span>,  <span>Gordon Brattborn</span>,  <span>Jennifer Quick</span>, <span>Melker Nilsson</span>, <span>Emrik Hoberg</span>, <span>Gordon Brattborn</span></p>
+            </div>
+          </div>
+
+          <div className="group-container" style={{ background: 'rgb(151, 49, 86)' }}>
+            <div className="group" style={{ background: 'rgb(151, 49, 86)' }}>
+              <div className="photo-container">
+                <div className="curtains"></div>
+                <img alt="Designers" src={require('../assets/team/programmers.jpeg')} />
+              </div>
+              <h2 className="caption">Programming</h2>
+              <p className="names">Left to right: <span>Anton Danielsson</span>, <span>Viktor Loby</span>, <span>Joel Dahl</span>, <span>Alfons Flystam</span>, <span>Erik Lagergren</span></p>
+              <p className="names">Absent: <span>Carl Borslöv</span></p>
+            </div>
+          </div>
         </div>
-        <div className="padded">
-          <ul id="team-list">
-            {
-              team.map((member, i) => (
-                <li className="member" key={i.toString()}>
-                  <div className="picture" style={{
-                    backgroundImage: `url(${member.image})`
-                  }}></div>
-                  <div className="about">
-                    <h2 className="name">
-                      {
-                        member.name
-                      }
-                    </h2>
-                    <p className="role">
-                      <div className="icon"
-                        style={{ backgroundImage: `url(${member.roleIcon})` }}>
-                      </div>
-                      <span className="text">
-                        {
-                          member.role
-                        }
-                      </span>
-                    </p>
-                    <a className="email-link" href={
-                      `mailto:${member.email}`
-                    }>Send me an ✉️</a>
-                  </div>
-                </li>
-              ))
-            }
-          </ul>
-        </div>
-      </div>
+      </div >
     )
   }
 }
